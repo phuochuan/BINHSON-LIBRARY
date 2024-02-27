d18038f92bce036d16b1fab206773466773ffd0b
huan <phbinhthuongtam@gmail.com>
2024-02-27 22:38:28 +0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	config-repository/api-gateway/application.properties

--- document-service/src/main/java/com/library/binhson/documentservice/rest/DemoController.java
+++ document-service/src/main/java/com/library/binhson/documentservice/rest/DemoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/demo")
+@RequestMapping("/api/v1/document-service/demo")
 public class DemoController {
     @GetMapping({"","/"})
     public ResponseEntity<?> demo() {return ResponseEntity.ok("ok");}
